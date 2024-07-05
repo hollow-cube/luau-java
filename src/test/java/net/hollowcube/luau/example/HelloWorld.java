0afc884ad675069270a2acc30ff015aea111d865
mworzala <mattheworzala@gmail.com>
2024-07-05 13:26:36 -0400

    fix: better hello world example

--- src/test/java/net/hollowcube/luau/example/HelloWorld.java
+++ src/test/java/net/hollowcube/luau/example/HelloWorld.java
@@ -16,8 +16,13 @@ public class HelloWorld {
             state.openLibs(); // Open all libraries
             state.sandbox(); // Sandbox the global state so it cannot be edited by a script
 
-            state.load("helloworld.luau", bytecode); // Load the script into the VM
-            state.pcall(0, 0); // Eval the script
+            var thread = state.newThread();
+            thread.sandboxThread(); // Create a mutable global env for scripts to use
+
+            thread.load("helloworld.luau", bytecode); // Load the script into the VM
+            thread.pcall(0, 0); // Eval the script
+
+            state.pop(1); // Pop the thread off the stack
         } finally {
             // Always remember to close the state when you're done with it, or you will leak memory.
             state.close();
